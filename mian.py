fdd09d065370bbc6d9288eb0b72d4f94ece2cf04
m7med077m <135227322+m7med077m@users.noreply.github.com>
2025-07-05 17:52:01 +0300

    Update mian.py

--- mian.py
+++ mian.py
@@ -94,10 +94,7 @@ def extract_video_link(result):
                 return obj
         return None
     return recursive_search(result)
-    
-@app.on_message(filters.private)
-async def clear_keyboard(client, message):
-    await message.reply("Keyboard removed ✅", reply_markup=ReplyKeyboardRemove())
+
 # === Message Handlers ===
 @app.on_message(filters.command("start") & filters.private)
 def start_cmd(client, message):
